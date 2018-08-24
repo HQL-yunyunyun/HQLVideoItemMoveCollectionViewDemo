6b58e70755d2ca4379ee13f03d75b0cdddca0685
HQL-yunyunyun <he84965092@163.com>
2018-08-24 16:54:51 +0800

    init

---
+++ HQLVideoItemMoveCollectionViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HQLVideoItemMoveCollectionViewDemo
+//
+//  Created by 何启亮 on 2018/8/24.
+//  Copyright © 2018年 hql_personal_team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
